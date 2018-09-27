938d81bf1138a7ed4d453f43d45af5f0828d3f56
missisol <missisololol@gmail.com>
2018-09-28 01:43:30 +0300

    В скрипт к странице checkout добавлена работа с формой регистрации пользователя.

--- src/script/checkout.js
+++ src/script/checkout.js
@@ -1,3 +1,53 @@
+// Правила проверки данных в полях формы регистрации
+var rules = {
+  name: /[a-zа-я]+/i,
+  password: /[\wа-я]{6}/,
+  creditCard: /^\d{7}-\d{4}-\d{6}-\d{3}$/,
+  email: /^[a-z0-9]+[.-]?[a-z0-9]+@[a-z]+\.[a-z]+/i,
+};
+
+// Подсказки для пользователя при неуспешной валидации полей формы регистрации
+var message = {
+  empty: 'Поле не должно быть пустым',
+  name: 'Поле должно содержать только буквы',
+  password: 'Длина пароля должна быть не меньше 6 символов',
+  creditCard: 'Введите номер карты в формате 1234567-1234-123456-123',
+  email: 'Введите e-mail в формате mymail@mail.ru или my.mail@mail.ru или my_mail@mail.ru',
+};
+
+/**
+ * В случае неуспешной валидации полей формы регистрации устанавливает класс 'invalid'
+ * и создает элемент с подсказкой для правильного заполнения полей
+ * @param {string} message - подсказка для пользователя
+ * @param {HTMLElement} inputEl - Поле, по которому валидация неуспешна 
+ */
+function setInvalidField(message, inputEl) {
+  $(inputEl).addClass('invalid');
+  var $hintWrap = $(inputEl).next('.invalid-feedback');
+  if ($hintWrap.length === 0) {
+    $hintWrap = $('<div />', {class:'invalid-feedback'}).text(message);
+    $hintWrap.insertAfter(inputEl);
+  } else {
+    $hintWrap.text(message);
+  }
+}
+
+// Записывает данные формы регистрации в базу данных
+function sendValues() {
+  var signUpFields = $('.signUpForm').serialize();
+  $.ajax({
+    url: 'http://localhost:3000/reg',
+    type: 'POST',
+    data: signUpFields,
+    success: function() {
+      console.log('Регистрация прошла успешно');
+    },
+    error: function() {
+      console.log('Ошибка регистрации');
+    }
+  });
+}
+
 (function($) {
   $(function() {
     // Создаем карусель товаров в шапке.
@@ -8,5 +58,44 @@
       slidesToShow: 1,
       adaptiveHeight: true
     });
+
+    $('.overlay').css('display', 'none');
+    $('.signUpForm').css('display', 'none');
+
+
+    // По клику на кнопку "continue" вызываем форму регистрации
+    $('.inputSubmitLeft').on('click', function(e) {
+      $('.overlay').css('display', 'block');
+      $('.signUpForm').css('display', 'flex');
+      e.preventDefault();
+    });
+
+    // По клику на подложку или крестик закрываем форму и убираем подложку
+    $('.signUpForm__close, .overlay').on('click', function() {
+      $('.overlay, .signUpForm').css('display', 'none');
+    });
+
+    // Валидирует форму регистрации перед отправкой
+    $('.signUpForm').on('click', '.signUpForm__submit', function(e) {
+      $.each(rules, function(index, rule) {
+        var mess;
+        var $fields = $('[data-validation_rule=' + index + ']');
+        $fields.each(function(key, field) {
+         if(rule.test(field.value)) {
+           $(field).removeClass('invalid');
+           $(field).next('.invalid-feedback').remove();
+          } else if (field.value !== '') {
+            mess = message[index];
+            setInvalidField(mess, field);
+          } else {
+            mess = message.empty;
+            setInvalidField(mess, field);
+          }
+        });
+      });
+       e.preventDefault();
+       sendValues();
+    });
+
   });
 })(jQuery);
